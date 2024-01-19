baa8eabf245ce7347cb47eb6d8261b5493c73e83
Vinh Luong <luonghoangvinh@gmail.com>
2024-01-19 14:51:35 -0800

    revert from Develop

--- WebAdmin/Models/ErrorViewModel.cs
+++ WebAdmin/Models/ErrorViewModel.cs
@@ -1,5 +1,8 @@
 namespace WebAdmin.Models
 {
+    /// <summary>
+    ///  Add Comment that only display on Staging branch
+    /// </summary>
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
